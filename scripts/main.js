5ac6f0b011c0a8854bbb567ac8f4a98b83ad9150
Tyler White <7670501@isd535.org>
2022-03-02 11:11:50 -0600

    listens for hover and start of image change logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,29 @@
-// Put your JavaScript here
+const IMAGE_SELECTOR = '[data-image-role="target"]';
+const IMAGE_CHOICE = '[data-image-role="trigger"]';
+
+let imgArray = [].slice.call(document.querySelectorAll(IMAGE_SELECTOR));
+let linkArray = [].slice.call(document.querySelectorAll(IMAGE_CHOICE));
+
+function getImage(link) {
+    'use strict';
+    return link.getAttribute('data-image-url');
+}
+
+function rippleEffect(inpt) {
+    for (let i = inpt - 1; i >= 0; i--) {
+       
+    }
+    for (let j = inpt + 1; j <= linkArray.length; j++) {
+        
+    }
+}
+
+function addHoverListener(img, num) {
+    'use strict';
+    img.addEventListener("mouseenter", rippleEffect(num))
+}
+
+function initializeEvents() {
+    'use strict';
+    linkArray.forEach(addHoverListener);
+}
